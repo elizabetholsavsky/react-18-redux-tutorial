3ed39513b3610788427a71f0c3cf028f6d3404f1
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-03 17:14:01 -0500

    change title and deploy using netlify

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -6,14 +6,16 @@ import {books} from './books';
 import Book from './Book';
 
 const BookList = () => {
-
     return (
+      <>
+      <h1>Amazon Best Sellers</h1>
         <section className="book-list">
           {books.map( (book, index) => {
             const {img ,title, author, id} = book
             return <Book {...book} key={book.id} number={index}/>
           })} 
         </section>
+      </>    
     );
 }
 
