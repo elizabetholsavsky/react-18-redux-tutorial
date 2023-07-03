1c69c69349a2f3570ac47311a06efadc4c33176e
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-03 16:04:52 -0500

    review js features, props and destructuring in const and funct param, children prop, key prop, spread op, and forms/events

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,38 +1,44 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+
+const books = [
+{
+  title: 'Fourth Wing',
+  author: 'Rebecca Yarros',
+  img: './images/book1.jpg',
+  id: 1,
+},
+{
+  title: 'The Housemaid',
+  author: 'Freida McFadden',
+  img: 'https://m.media-amazon.com/images/I/41zWWzbiHpL._SX331_BO1,204,203,200_.jpg',
+  id: 2,
+},
+];
+
 
 const BookList = () => {
     return (
         <section className="book-list">
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
+          {books.map( (book) => {
+            const {img ,title, author, id} = book
+            return <Book {...book} key={book.id}/>
+          })} 
         </section>
     );
 }
 
-const Book = () => {
+const Book = ({img, title, author }) => {
     return (
         <article className="book">
-            <Image/>
-            <Title/>
-            <Author/>
+          <img src={img} alt={title}/>
+          <h2> {title} </h2>
+          <h4> {author} </h4> 
         </article>
     )
 }
-
-const Image = () => <img src='./images/book1.jpg' alt='Fourth Wing'/>;
-const Title = () => {
-    return <h2> Fourth Wing </h2>;
-};
-const Author = () => {
-    return (
-<h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
-    Rebecca Yarros</h4> 
-    );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
