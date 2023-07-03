5e04faeb223168e45818b9297582e40d66a30bdb
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-03 16:56:53 -0500

    separate compnent and var into files, import/export, store img locally in src folder for optimization

---
+++ tutorial/src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+    {
+      title: 'Fourth Wing',
+      author: 'Rebecca Yarros',
+      img: img1,
+      id: 1,
+    },
+    {
+      title: 'The Housemaid',
+      author: 'Freida McFadden',
+      img: img2,
+      id: 2,
+    },
+    {
+      title: 'Iron Flame',
+      author: 'Rebecca Yarros',
+      img: img3,
+      id: 3,
+    },
+    ];
+    
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,42 +2,19 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-{
-  title: 'Fourth Wing',
-  author: 'Rebecca Yarros',
-  img: './images/book1.jpg',
-  id: 1,
-},
-{
-  title: 'The Housemaid',
-  author: 'Freida McFadden',
-  img: 'https://m.media-amazon.com/images/I/41zWWzbiHpL._SX331_BO1,204,203,200_.jpg',
-  id: 2,
-},
-];
-
+import {books} from './books';
+import Book from './Book';
 
 const BookList = () => {
+
     return (
         <section className="book-list">
-          {books.map( (book) => {
+          {books.map( (book, index) => {
             const {img ,title, author, id} = book
-            return <Book {...book} key={book.id}/>
+            return <Book {...book} key={book.id} number={index}/>
           })} 
         </section>
     );
-}
-
-const Book = ({img, title, author }) => {
-    return (
-        <article className="book">
-          <img src={img} alt={title}/>
-          <h2> {title} </h2>
-          <h4> {author} </h4> 
-        </article>
-    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
