35df95136dae6e2a2b0a142d35d299e6d2d455da
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-02 20:57:13 -0500

    review jsx rules, nesting components, img, inline css, began booklist project

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,12 +1,39 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
-
-function Greeting() {
-    return <><h2>My First Component</h2></>
-    
+const BookList = () => {
+    return (
+        <section className="book-list">
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    );
 }
+
+const Book = () => {
+    return (
+        <article className="book">
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>
+    )
+}
+
+const Image = () => <img src='./images/book1.jpg' alt='Fourth Wing'/>;
+const Title = () => {
+    return <h2> Fourth Wing </h2>;
+};
+const Author = () => {
+    return (
+<h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+    Rebecca Yarros</h4> 
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting/>)
+root.render(<BookList/>)
